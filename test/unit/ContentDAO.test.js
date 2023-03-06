09d6326c7d713247e1505909f3466795cb6c7fdb
Ognjen <cryptognjen@gmail.com>
2023-03-06 18:40:58 +0100

    added tests

--- test/unit/ContentDAO.test.js
+++ test/unit/ContentDAO.test.js
@@ -1,13 +1,25 @@
 const { inputToConfig } = require("@ethereum-waffle/compiler");
 const { getContractFactory } = require("@nomiclabs/hardhat-ethers/types");
 const { assert } = require("chai");
-const { deployments, getNamedAccounts, ethers, network } = require("hardhat");
+const { utils } = require("ethers");
+const {
+  deployments,
+  getNamedAccounts,
+  ethers,
+  network,
+  hardhatArguments,
+} = require("hardhat");
 const { localNetworks } = require("../../helper.hardhat.config");
 
 !localNetworks.includes(network.name)
   ? describe.skip
   : describe("ContentDAO", () => {
-      let deployer, creator, contentDAO, MockV3Aggregator;
+      let deployer,
+        creator,
+        contentDAO,
+        MockV3Aggregator,
+        timestamp,
+        latestTimestamp;
 
       beforeEach(async () => {
         await deployments.fixture(["all"]);
@@ -29,11 +41,11 @@ const { localNetworks } = require("../../helper.hardhat.config");
           assert.equal(priceFeedAddress, mockAddress);
         });
         it("Sets last timestamp as the block timestamp", async () => {
-          const lastTimestamp = await contentDAO.getLatestTimestamp();
+          latestTimestamp = await contentDAO.getLatestTimestamp();
           const provider = ethers.provider;
           const block = await provider.getBlock(2);
-          const timestamp = await block.timestamp;
-          assert.equal(timestamp, lastTimestamp);
+          timestamp = await block.timestamp;
+          assert.equal(timestamp, latestTimestamp);
         });
         it("Sets mint status to open", async () => {
           const mintStatus = await contentDAO.getMintStatus();
@@ -55,6 +67,40 @@ const { localNetworks } = require("../../helper.hardhat.config");
         it("Sets token symbol correctly", async () => {
           const tokenSymbol = await contentDAO.symbol();
           assert.equal(tokenSymbol, "CT");
+        });
+      });
+
+      describe("ETH to USD converter", () => {
+        it("Converts ETH value entered by user to USD", async () => {
+          const ethAmount = await contentDAO.convertToUsd(
+            ethers.utils.parseEther("1")
+          );
+          const ethAmountToStr = ethAmount.toString();
+          const [, LatestPrice] = await MockV3Aggregator.latestRoundData();
+          const LatestPriceToString = LatestPrice.toString() * "10000000000";
+          assert.equal(ethAmountToStr, LatestPriceToString);
+        });
+      });
+      describe("CheckUpkeep", () => {
+        let contractWithSigner;
+        beforeEach(async () => {
+          const signer = (await ethers.getSigners())[3];
+          contractWithSigner = contentDAO.connect(signer);
+        });
+
+        it("Checks if enought time has passed, has holders and is open", async () => {
+          const mintingDuration = await contentDAO.getMintDuration();
+          await contractWithSigner.ctTokenMint({
+            value: ethers.utils.parseEther("0.0075"),
+          });
+          let [upkeepNeeded] = await contentDAO.checkUpkeep("0x");
+          assert.equal(upkeepNeeded, false);
+          await network.provider.send("evm_increaseTime", [
+            mintingDuration.toNumber() + 5,
+          ]);
+          await network.provider.send("evm_mine", []);
+          [upkeepNeeded] = await contentDAO.checkUpkeep("0x");
+          assert.equal(upkeepNeeded, true);
         });
       });
     });
