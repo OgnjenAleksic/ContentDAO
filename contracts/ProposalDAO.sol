32e617712ad08853195f1bcc35b808f15e09122e
Ognjen <cryptognjen@gmail.com>
2023-03-15 18:10:37 +0100

    removed functionality

--- contracts/ProposalDAO.sol
+++ contracts/ProposalDAO.sol
@@ -4,35 +4,26 @@ pragma solidity ^0.8.0;
 
 import "./ContentDAO.sol";
 
+error Proposal_Status_Is_Closed();
+error You_Dont_Hold_CT();
+error Proposal_Has_Been_Finalized();
+
 contract ProposalDAO is ContentDAO {
     struct Proposal {
         string proposalName;
-        uint256 yesVotes;
-        uint256 noVotes;
-        bool finalized;
+        uint256 votes;
     }
+
+    struct Voter {
+        bool voted;
+    }
+
+    Proposal[3] proposals;
+
+    Proposal winningProposal;
 
     constructor(
         address aggregatorV3Address,
         address creator
     ) ContentDAO(aggregatorV3Address, creator) {}
-
-    uint numberOfProposals;
-
-    mapping(uint => Proposal) numberOfProposalToProposal;
-
-    //Proposing a vote
-
-    function proposeVote(string memory _proposalName) external onlyCreator {
-        numberOfProposalToProposal[numberOfProposals] = Proposal(
-            _proposalName,
-            0,
-            0,
-            false
-        );
-
-        numberOfProposals++;
-    }
-
-    //Voting
 }
